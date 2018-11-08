b4ccf0c907885865515e788b68d08fc59464a8fc
Raul <raulcc50@gmail.com>
2018-11-07 22:45:58 -0200

    FrontController e Command (Cabeçalhos)

---
+++ src/main/java/com/ifpb/controller/FrontController.java
@@ -0,0 +1,18 @@
+package com.ifpb.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/controller")
+
+public class FrontController extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
